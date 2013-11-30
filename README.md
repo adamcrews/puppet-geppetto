a395d1d44aaca2d381ed554b048ae480a97757c1
Adam Crews <adam@puppetlabs.com>
2013-11-29 17:49:34 -0800

    Cleanup and docs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/boxen/puppet-geppetto.png?branch=master)](https://travis-ci.org/boxen/puppet-geppetto)
+[![Build Status](https://travis-ci.org/adamcrews/puppet-geppetto.png?branch=master)](https://travis-ci.org/adamcrews/puppet-geppetto)
 
 ## Usage
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,32 @@
-class geppetto {
+# == Class: geppetto
+#
+# A boxen module to install geppetto
+#
+# === Parameters
+#
+# [*version]
+#   The version string to download and install
+#
+# === Variables
+#
+# [*arch]
+#   The system architecture, ie 32 vs 64 bit
+#
+# === Examples
+#
+# include geppetto
+#
+# === Authors
+#
+# Adam Crews <adam@puppetlabs.com>
+#
+# === Copyright
+#
+# Copyright 2013 Adam Crews, unless otherwise noted.
+#
+class geppetto (
   $version = '4.0.0-R201310140657'
+) {
   $arch = $::architecture ? {
     'x86_64' => 'x86_64',
     default  => 'x86',
