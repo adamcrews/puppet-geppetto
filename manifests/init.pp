0567c58d42a6a7e8d3eeccfd6e8bf8a516b9d215
Adam Crews <adam@puppetlabs.com>
2014-02-25 11:08:44 -0800

    Can't make it latest... grr..

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,13 +16,19 @@
 #
 # include geppetto
 #
+# === Known issues
+#
+# Since this useses the compressed_app provider,
+# the package is not upgradeable.  You must uninstall
+# any old versions before installing a new version.
+#
 # === Authors
 #
 # Adam Crews <adam@puppetlabs.com>
 #
 # === Copyright
 #
-# Copyright 2013 Adam Crews, unless otherwise noted.
+# Copyright 2014 Adam Crews, unless otherwise noted.
 #
 class geppetto (
   $version = '4.1.0-R201402150632'
@@ -33,7 +39,7 @@ class geppetto (
   }
 
   package { 'geppetto':
-    ensure   => latest,
+    ensure   => installed,
     source   => "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.${arch}-${version}.zip",
     provider => compressed_app,
   }
